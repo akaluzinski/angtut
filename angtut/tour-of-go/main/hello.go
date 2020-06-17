aec6dc80549645073db885e230c8a631ac25ccc5
Adrian Kaluzinski <adrian@kaluzinski.eu>
2020-06-17 18:55:04 +0200

    First golang function

--- angtut/tour-of-go/main/hello.go
+++ angtut/tour-of-go/main/hello.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func randomPrint() {
+	rand.Seed(time.Now().Unix())
+	fmt.Println("Some 'random' number: ", rand.Intn(10000))
+}
 
 func main() {
-	fmt.Printf("Hello, world!")
+	fmt.Println("Hello, world!")
+	fmt.Println("The time is: ", time.Now())
+
+	randomPrint()
+
+	fmt.Println(add(6, 7))
 }
