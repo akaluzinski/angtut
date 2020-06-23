1153476103850246d174a388b0635262f080fc30
Adrian Kaluzinski <adrian@kaluzinski.eu>
2020-06-23 17:34:35 +0200

    Golang HelloWorld using net/http

--- angtut/tour-of-go/main/hello.go
+++ angtut/tour-of-go/main/hello.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+	"log"
+	"net/http"
 )
 
-func add(x int, y int) int {
-	return x + y
+const message = `{"message": "Hello World"}`
+
+const ApplicationJson = "application/json"
+
+type server struct{}
+
+func (s *server) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	setContentType(responseWriter, ApplicationJson)
+	setOKStatus(responseWriter)
+	responseWriter.Write([]byte(message))
 }
 
-func randomPrint() {
-	rand.Seed(time.Now().Unix())
-	fmt.Println("Some 'random' number: ", rand.Intn(10000))
+func setContentType(writer http.ResponseWriter, contentType string) {
+	writer.Header().Set("Content-Type", contentType)
+}
+
+func setOKStatus(writer http.ResponseWriter) {
+	writer.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println("The time is: ", time.Now())
-
-	randomPrint()
-
-	fmt.Println(add(6, 7))
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
