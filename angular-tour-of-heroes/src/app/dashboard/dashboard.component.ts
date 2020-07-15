c1a5feffe537e339523368cecdfd87fb0daf6efa
Adrian Kaluzinski <adrian@kaluzinski.eu>
2020-07-15 21:21:06 +0200

    Create top heroes dashboard

---
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class DashboardComponent implements OnInit {
+
+  static readonly MAX_HEROES = 4;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(0, DashboardComponent.MAX_HEROES));
+  }
+}
