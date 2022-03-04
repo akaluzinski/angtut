a18b5938ab6a930f82f1d173a52af63b953001e6
Adrian Kaluzinski <kaluza@gmail.com>
2022-03-04 16:31:34 +0100

    Add color and X transition to the box

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(50px)'
+      })),
+      transition('normal <=> highlighted', animate(300))
+    ])
+  ]
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAdd(item: string): void {
+    this.list.push(item);
+  }
+
+  onDelete(item: string): void {
+  }
+
+  onAnimate() {
+    this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+  }
 }
